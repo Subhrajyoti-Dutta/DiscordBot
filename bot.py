cd907d2956bdeaa725f6c2d5b9cf1be77fa80fa9
Subhrajyoti <duttajyotisubhra@gmail.com>
2021-08-10 04:17:55 +0530

    established the bot

---
+++ bot.py
@@ -0,0 +1,36 @@
+import os
+import message as msg
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to Pichavaram Event server!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.lower() == 'hi':
+        response = msg.reply(message)
+        await message.channel.send(response)
+
+client.run(TOKEN)
---
+++ message.py
@@ -0,0 +1,8 @@
+def reply(msg):
+    if msg.content.lower() == "hi":
+        response = f"Hello {message.author}"
+
+    else:
+        response = "I don't understand"
+
+    return response
