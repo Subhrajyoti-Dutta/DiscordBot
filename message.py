f0162a59d82e1965b39616c002d3654da86635ed
Subhrajyoti <duttajyotisubhra@gmail.com>
2021-08-10 04:45:49 +0530

    minor changes

--- message.py
+++ message.py
@@ -1,6 +1,9 @@
 def reply(msg):
     if msg.content.lower() == "hi":
-        response = f"Hello {message.author}"
+        response = f"Hello {msg.author}"
+
+    elif msg.content.lower() == "1":
+        pass
 
     else:
         response = "I don't understand"
